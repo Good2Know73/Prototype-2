d917e1dc46051d38d23b9840cd355bd416cc58ad
Good2Know73 <bainhastie@gmail.com>
2020-01-09 10:59:04 -0800

    Finished
    Finished Game

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -15,11 +15,13 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // If an object ges ast the players view in the game, remove that object
         if (transform.position.z > 30)
         {
             Destroy(gameObject);
         } else if (transform.position.z < lowerBound)
         {
+           Debug.Log("Game Over!");
             Destroy(gameObject);
         }
     }
